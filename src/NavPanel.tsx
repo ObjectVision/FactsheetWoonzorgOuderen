10677f63a36b345d3e2c221cf51fb762f652241f
NieneB <niene.boeijen@gmail.com>
2025-07-23 14:41:59 +0200

    logo added

--- src/NavPanel.tsx
+++ src/NavPanel.tsx
@@ -4,6 +4,7 @@ function NavPanel({currentNeighborhood, setCurrentNeighborhood}) {
 
   return (
     <div id="nav-panel">
+        <img src="./logo.gif"/>
     <h1>Gemeente Sittard-Geleen </h1>
     <Map currentNeighborhood={currentNeighborhood} setCurrentNeighborhood={setCurrentNeighborhood}/>
     <h3>{currentNeighborhood}</h3>
