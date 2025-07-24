834b6578e22e5365b16b428314c269974dde14bd
eoudejans <eoudejans@objectvision.nl>
2025-07-25 00:43:16 +0200

    Added example mvt layer.

--- src/Map.tsx
+++ src/Map.tsx
@@ -7,6 +7,7 @@ import {BitmapLayer, GeoJsonLayer} from '@deck.gl/layers';
 import {TileLayer} from '@deck.gl/geo-layers';
 //import { } from '@loaders.gl/flatgeobuf';
 //import {FlatGeoBufLoader} from '@loaders.gl/flatgeobuf';
+import {MVTLayer} from '@deck.gl/geo-layers';
 
 const INITIAL_VIEW_STATE:any = {
   longitude: 5.844702066665236,
@@ -55,6 +56,41 @@ function Map() {
               ];
             }
           }),
+          /*new MVTLayer({
+            id: 'MVTLayer',
+            data: [
+              'https://tiles-a.basemaps.cartocdn.com/vectortiles/carto.streets/v1/{z}/{x}/{y}.mvt'
+            ],
+            minZoom: 0,
+            maxZoom: 14,
+            getFillColor: (f: any) => {
+              switch (f.properties.layerName) {
+                case 'poi':
+                  return [255, 0, 0];
+                case 'water':
+                  return [120, 150, 180];
+                case 'building':
+                  return [218, 218, 218];
+                default:
+                  return [240, 240, 240];
+              }
+            },
+            getLineWidth: (f: any) => {
+              switch (f.properties.class) {
+                case 'street':
+                  return 6;
+                case 'motorway':
+                  return 10;
+                default:
+                  return 1;
+              }
+            },
+            getLineColor: [192, 192, 192],
+            getPointRadius: 2,
+            pointRadiusUnits: 'pixels',
+            stroked: false,
+            picking: true
+          }),*/
           new GeoJsonLayer({
             id: 'GeoJsonLayer', 
             data: wijken,
