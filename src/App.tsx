4511f3dc39435b190033fe3aad25ba77722ec013
eoudejans <eoudejans@objectvision.nl>
2025-07-24 13:34:36 +0200

    Initial deck.gl fullscreen map

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import NavPanel from "./NavPanel";
 import Factsheet from "./Factsheet";
+import Map from "./Map"
 
 function App() {
   const [currentNeighborhood, setCurrentNeighborhood] = useState<string>("Test wijk");
@@ -10,13 +11,16 @@ function App() {
     console.log(currentNeighborhood);
   }, [currentNeighborhood]);
 
-  return (
-    <div id="app-container">
+      /*<div id="app-container">
       <NavPanel
         currentNeighborhood={currentNeighborhood}
         setCurrentNeighborhood={setCurrentNeighborhood}
       />
       <Factsheet currentNeighborhood={currentNeighborhood} />
+    </div>*/
+  return (
+    <div id="app-main-area">
+      <Map/>
     </div>
   );
 }
--- src/Map.tsx
+++ src/Map.tsx
@@ -1,52 +1,57 @@
-import maplibregl from "maplibre-gl";
-import "maplibre-gl/dist/maplibre-gl.css";
 import { useEffect, useState } from "react";
-import geodata from "/cbs_wijken_limburg.json?url";
+//import geodata from "/cbs_wijken_limburg.json?url";
+import {DeckGL} from '@deck.gl/react';
+import {MapView, Deck, OrthographicView} from '@deck.gl/core';
+import {BitmapLayer, PathLayer} from '@deck.gl/layers';
+import {TileLayer} from '@deck.gl/geo-layers';
 interface MapProps {
   currentNeighborhood: string;
   setCurrentNeighborhood: (e: string) => void;
 }
 
-function Map({ currentNeighborhood, setCurrentNeighborhood }: MapProps) {
+function Map() {
   const [mapReady, setMapReady] = useState(false);
 
   useEffect(() => {
-    if (!mapReady) return;
+    const deck = new Deck({
+      canvas: 'deck-canvas',
+      //views: new OrthographicView(),
+      initialViewState: {
+        longitude: 5.606440797330272,
+        zoom: 7,
+        latitude: 52.062958234585004,
+        pitch: 0,
+        bearing: 0
+      },
+      controller: true,
+      layers: [
+          new TileLayer<ImageBitmap>({
+            data: ['https://tile.openstreetmap.org/{z}/{x}/{y}.png'],
+            maxRequests: 20,
+            pickable: true,
+            autoHighlight: false,
+            highlightColor: [60, 60, 60, 40],
+            minZoom: 0,
+            maxZoom: 19,
+            tileSize: 256,
+            zoomOffset: -0.9,
+            renderSubLayers: props => {
+              const [[west, south], [east, north]] = props.tile.boundingBox;
+              const {data, ...otherProps} = props;
 
-    const map = new maplibregl.Map({
-      container: "central-map",
-      zoom: 8,
-      hash: true,
-      center: [5.879, 51.173],
-    });
-
-    map.on("load", () => {
-      fetch("./assets/cbs_wijken_limburg.json")
-      .then(response => JSON.parse(response))
-      .then(data => console.log(data))
-
-      map.addSource("limburg", {
-        type: "geojson",
-        data: geodata,
+              return [
+                new BitmapLayer(otherProps, {
+                  image: data,
+                  bounds: [west, south, east, north]
+                })
+              ];
+            }
+          })
+        ]
       });
+  });
 
-      map.addLayer({
-        id: "limburg",
-        source: "limburg",
-        type: "fill",
-        paint: {
-          "fill-color": "#eeff00",
-        },
-      });
-    });
-
-    map.on("click", (e) => {
-      console.log(e);
-      setCurrentNeighborhood("test wijk nr 2");
-    });
-  }, [mapReady]);
-
-  return <div ref={() => setMapReady(true)} id="central-map" />;
+  return <canvas id="deck-canvas"></canvas>;
 }
 
 export default Map;
