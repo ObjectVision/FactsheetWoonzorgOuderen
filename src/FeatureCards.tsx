9274c6f682ebd2e92eb56385bb543d2bca700261
eoudejans <eoudejans@objectvision.nl>
2025-07-26 17:41:37 +0200

    Added closebutton to FeatureCards.

--- src/FeatureCards.tsx
+++ src/FeatureCards.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent, Typography, Box } from '@mui/material';
-
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
 // Optional: define GeoJSON.Feature type if not available
 //type Feature = GeoJSON.Feature;
 
@@ -9,12 +10,15 @@ interface ChildProps {
   setSelectedPolygons: React.Dispatch<React.SetStateAction<GeoJSON.Feature[]>>;
 }
 
+
+
 export default function FeatureCards({ selectedPolygons, setSelectedPolygons }: ChildProps) {
-  //const shownFeatures = features.slice(-3); // Show last 3 selected
+  if (!selectedPolygons)
+    return;
 
-    if (!selectedPolygons)
-        return;
-
+  const handleRemove = (indexToRemove: number) => {
+    setSelectedPolygons(prev => prev.filter((_, i) => i !== indexToRemove));
+  };
   return (
     <Box
       sx={{
@@ -29,8 +33,16 @@ export default function FeatureCards({ selectedPolygons, setSelectedPolygons }: 
       }}
     >
       {selectedPolygons.map((feature:GeoJSON.Feature, idx:number) => (
-        <Card key={idx} sx={{ minWidth: 200, maxWidth:200}}>
+        <Card key={idx} sx={{ minWidth: 200, maxWidth:200, position: 'relative'}}>
+          <IconButton
+            size="small"
+            onClick={() => handleRemove(idx)}
+            sx={{ position: 'absolute', top: 4, right: 4 }}
+          >
+          <CloseIcon fontSize="small" />
+          </IconButton>
           <CardContent>
+            
             <Typography variant="h6">{feature.properties!.naam}</Typography>
             <Typography variant="body2" color="text.secondary">
                 <strong>WK_CODE:</strong> {feature.properties!.WK_CODE}
