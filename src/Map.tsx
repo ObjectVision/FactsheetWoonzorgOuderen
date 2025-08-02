0e28241f0cb4333faa09ee17a768b735cb02ca7d
eoudejans <eoudejans@objectvision.nl>
2025-08-02 02:22:30 +0200

    Added geoarrow test

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import wijken from "./assets/cbs_wijken_limburg.json?url";
-//import bag_panden from "./assets/bag_panden_Limburg.arrow?url";
+import bag_panden from "./assets/bag_pand_Limburg_uncompressed.arrow?url";
 import {DeckGL} from '@deck.gl/react';
 import type {PickingInfo} from '@deck.gl/core';
 import {BitmapLayer, GeoJsonLayer} from '@deck.gl/layers';
 import {TileLayer} from '@deck.gl/geo-layers';
+import { GeoArrowPolygonLayer } from "@geoarrow/deck.gl-layers";
+import * as arrow from "apache-arrow";
 
 const INITIAL_VIEW_STATE:any = {
   longitude: 5.844702066665236,
@@ -17,6 +19,7 @@ const INITIAL_VIEW_STATE:any = {
 function getTooltip({object}: PickingInfo) {
   if (object == undefined)
     return null;
+  return null;
   
   return object && {
     html: `<div>${object.properties.naam}</div>`,
@@ -28,6 +31,15 @@ function getTooltip({object}: PickingInfo) {
   };
 }
 
+/////////////////
+
+
+
+
+
+
+/////////////////
+
 interface ChildProps {
   selectedPolygons: GeoJSON.Feature[];
   setSelectedPolygons: React.Dispatch<React.SetStateAction<GeoJSON.Feature[]>>;
@@ -35,6 +47,25 @@ interface ChildProps {
 
 function Map({ selectedPolygons, setSelectedPolygons }: ChildProps) {
   const [isHovering, setIsHovering] = useState(false);
+  const [table, setTable] = useState<arrow.Table | null>(null);
+  
+  useEffect(() => {
+    // declare the data fetching function
+    const fetchData = async () => {
+      const data = await fetch(bag_panden);
+      const buffer = await data.arrayBuffer();
+      const table = arrow.tableFromIPC(buffer);
+      const table2 = new arrow.Table(table.batches.slice(0, 10));
+      //window.table = table2;
+      setTable(table2);
+    };
+
+    if (!table) {
+      fetchData().catch(console.error);
+    }
+  });
+
+
   const background_layer = new TileLayer<ImageBitmap>({
             data: ['https://tile.openstreetmap.org/{z}/{x}/{y}.png'],
             id: 'background_layer',
@@ -106,10 +137,32 @@ function Map({ selectedPolygons, setSelectedPolygons }: ChildProps) {
     getLineColor: [200, 0, 0, 200],
 });
 
+    const arrow_layer =  new GeoArrowPolygonLayer({
+        id: "geoarrow-polygons",
+        stroked: true,
+        filled: true,
+        data: table!,
+        getFillColor: [0, 100, 60, 160],
+        getLineColor: [0, 0, 0],
+        lineWidthMinPixels: 0.5,
+        extruded: false,
+        wireframe: false,
+        // getElevation: 0,
+        pickable: true,
+        positionFormat: "XY",
+        _normalize: false,
+        autoHighlight: false,
+        // Note: change this version string if needed
+        earcutWorkerUrl: new URL(
+          "https://cdn.jsdelivr.net/npm/@geoarrow/geoarrow-js@0.3.0/dist/earcut-worker.min.js",
+        ),
+      });
+
   const layers = [
           background_layer,
           navigation_layer,
-          selection_layer
+          selection_layer,
+          arrow_layer
         ];
 
   return <DeckGL
