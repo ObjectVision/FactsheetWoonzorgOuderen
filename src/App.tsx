31599e473144d587502fb285bf3b6781af59af1d
eoudejans <eoudejans@objectvision.nl>
2025-11-13 12:02:33 +0100

    Added routing test for embedding in iframe tech.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import TitleBox from './Title.tsx'
 import RegionToggleButtons from './RegionToggleButtons.tsx'
 import sources_json from "./data/sources.json?url";
 import layers_json from "./data/layers.json?url";
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
 
 function App() {
   const [selectedPolygons, setSelectedPolygons] = useState<GeoJSON.Feature[]>([]);
@@ -22,22 +23,37 @@ function App() {
     fetchAppJsonFiles();
   }, []);
 
-  return (
-    <div>
-      <div id="details-area">
-        
-        <FeatureCards selectedPolygons={selectedPolygons} setSelectedPolygons={setSelectedPolygons} />
-      </div>
-      
-      <div id="details-area">
-        <RegionToggleButtons activeRegions={activeRegions} setActiveRegions={setActiveRegions}/>
-      </div>
-
-      <div id="map-area">
-
-        <Map  sourceJSON={sourceJSON} layerJSON={layerJSON} selectedPolygons={selectedPolygons} setSelectedPolygons={setSelectedPolygons} activeRegions={activeRegions}/>
-      </div>
+  const MapEmbed = () => (
+    <div style={{ width: "100%", height: "100vh", margin: 0, padding: 0 }}>
+      <Map  sourceJSON={sourceJSON} layerJSON={layerJSON} selectedPolygons={selectedPolygons} setSelectedPolygons={setSelectedPolygons} activeRegions={activeRegions}/>
     </div>
+  );
+  
+  return (
+    <Router>
+      <Routes>
+        <Route path = "/" element={
+          <div>
+            <div id="details-area">
+              
+              <FeatureCards selectedPolygons={selectedPolygons} setSelectedPolygons={setSelectedPolygons} />
+            </div>
+            
+            <div id="details-area">
+              <RegionToggleButtons activeRegions={activeRegions} setActiveRegions={setActiveRegions}/>
+            </div>
+
+            <div id="map-area">
+
+              <Map  sourceJSON={sourceJSON} layerJSON={layerJSON} selectedPolygons={selectedPolygons} setSelectedPolygons={setSelectedPolygons} activeRegions={activeRegions}/>
+            </div>
+          </div>}>
+        </Route>
+        
+        <Route path="/embed/map" element={<MapEmbed />} />
+
+    </Routes>
+    </Router>
   );
 }
 
