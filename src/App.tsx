37d0e6c23210d439b8ebe57fb11904d619e14bba
eoudejans <eoudejans@objectvision.nl>
2025-11-16 15:40:17 +0100

    Added shadcn ui + tailwind support

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import "./App.css";
 import Map from "./Map.tsx"
 import FeatureCards from './FeatureCards';
 import TitleBox from './Title.tsx'
+import { AppSidebar } from './components/app-sidebar.tsx'
 import sources_json from "./data/sources.json?url";
 import layers_json from "./data/layers.json?url";
-
+import { SidebarProvider } from "@/components/ui/sidebar"
 function App() {
   const [selectedPolygons, setSelectedPolygons] = useState<GeoJSON.Feature[]>([]);
   const [sourceJSON, setsourceJSON] = useState<JSON[]>();
@@ -25,6 +26,12 @@ function App() {
       <div id="details-area">
         <TitleBox title={"Wonen & Zorg Limburg"}/>
         <FeatureCards selectedPolygons={selectedPolygons} setSelectedPolygons={setSelectedPolygons} />
+      </div>
+
+      <div>
+        <SidebarProvider>
+          <AppSidebar/>
+        </SidebarProvider>
       </div>
       
       <div id="map-area">
